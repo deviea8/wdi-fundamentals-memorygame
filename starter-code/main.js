db6f1b22d8e421bae3b5d3cea45a0a120a5f8e6c
Erin Martin <emartin@EMARTIN.local>
2017-02-17 22:45:57 -0500

    Commented out card divs from HTML and adjusted main.js to create card divs with Javascript instead

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,9 +4,27 @@ var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
+/* 
+Commenting out if statements from prior exercise.
+
 if (cardOne === cardTwo) {
 	alert('You found a match!');
 }
 else {
 	alert('Sorry, try again.');
 }
+*/
+
+
+var gameBoard = document.getElementById('game-board');
+var createCards = function () {
+for (i=0; i<4; i++) { 
+  var card = document.createElement('div');
+  gameBoard.appendChild(card);
+  card.className='card';
+  }
+}
+
+window.addEventListener('load',createCards);
+
+
